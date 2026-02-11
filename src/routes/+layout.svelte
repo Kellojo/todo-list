b61be78c4e5b7cd6d3bfb4c5d4716049923b87e6
Kellojo <12833426+Kellojo@users.noreply.github.com>
2026-02-11 21:12:04 +0100

    Added new pwa manifest and favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import favicon from "$lib/assets/favicon.svg";
+  import favicon96 from "$lib/assets/favicon-96x96.png";
+  import faviconIco from "$lib/assets/favicon.ico";
+  import appleTouch from "$lib/assets/apple-touch-icon.png";
+  import manifest from "$lib/assets/site.webmanifest";
   import { onMount } from "svelte";
   import { Toaster } from "svelte-sonner";
 
@@ -35,6 +39,12 @@
 
 <svelte:head>
   <link rel="icon" href={favicon} />
+  <link rel="icon" type="image/png" href={favicon96} sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href={favicon} />
+  <link rel="shortcut icon" href={faviconIco} />
+  <link rel="apple-touch-icon" sizes="180x180" href={appleTouch} />
+  <meta name="apple-mobile-web-app-title" content="Todo List" />
+  <link rel="manifest" href={manifest} />
 </svelte:head>
 
 <div class="background"></div>
